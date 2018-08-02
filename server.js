64577a37867db3b4c21d8b9d6c42ad56172ff66b
Beata Mlicka <mlicka.beata@gmail.com>
2018-08-02 12:34:52 +0200

    server production settings added

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ var app = express();
 app.use(express.static('public'));
 
 app.listen(5000, function() {
-    console.log('Express server is up on port 5000');
+    //console.log('Express server is up on port 5000');
 });
